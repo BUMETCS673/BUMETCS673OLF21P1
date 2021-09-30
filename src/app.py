e45d5350199c873bcc23000403c42bc4be93fc78
grace303 <yanruz@bu.edu>
2021-09-29 21:52:06 -0700

    display a single recipe

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, render_template, request
 from spoon import searchRecipes
 
@@ -24,6 +25,15 @@ def showRecipes():
     results = searchRecipes(ingredients, diet, intolerances)
     # set second argument to pass the data
     return render_template('recipe.html', results=results)
+
+
+@app.route("/recipe/<recipe_id>")
+def getRecipeDetail(recipe_id):
+    API_KEY = '9e749e7df97047c38000f0f4addb64f9'
+    req = f'https://api.spoonacular.com/recipes/{recipe_id}/information?&apiKey={API_KEY}'
+    res = requests.get(req)
+    data = res.json()
+    return render_template('recipe_detail.html', recipe=data)
 
 # test module import
 if __name__ == "__main__":
