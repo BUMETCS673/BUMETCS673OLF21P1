940468bfaa76123e8052a243e7707104c82730a3
Kyle Mabry <68658192+kylemabry95@users.noreply.github.com>
2021-09-30 18:44:46 -0700

    Remove unused code and implement ingredient database and storage of ingredients

--- src/app.py
+++ src/app.py
@@ -1,21 +1,46 @@
+import random
+
 from flask import Flask, render_template, request
 from spoon import searchRecipes
-from findSpecificRecipe import searchForRecipe, getRecipeInformation
+from flask_sqlalchemy import SQLAlchemy
 
 # flask class instance
+
 app = Flask(__name__)
 
+# set up database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/spoon.db'
+db = SQLAlchemy(app)
+
+
+class Ingredient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ingredientName = db.Column(db.String(80), nullable=False)
+    ingredientQuantity = db.Column(db.String(120), nullable=False)
+    expirationDate = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return '<Id: %r>' % self.id + '<Ingredient Name: %r>' % self.ingredientName \
+               + '<Ingredient Quantity: %r>' % self.ingredientQuantity \
+               + '<Expiration Date: %r>\n' % self.expirationDate
+
+
 API_KEY = '9e749e7df97047c38000f0f4addb64f9'
+
 
 # / route will show our index template
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 # /about will show about page
 app.route("/about")
+
+
 def about():
     return render_template("about.html")
+
 
 @app.route("/recipe")
 def showRecipes():
@@ -23,25 +48,40 @@ def showRecipes():
     diet = request.args.get('diet')
     intolerances = request.args.get('intolerances')
     results = searchRecipes(ingredients, diet, intolerances)
-    print("ran wrong function")
+
     # set second argument to pass the data
     return render_template('recipe.html', results=results)
 
-@app.route("/getRecipeByName")
-def getRecipeByName():
-    recipe_name = request.args.get('recipeName')
-    recipe_results = searchForRecipe(recipe_name)
 
-    return render_template('recipe.html', results=recipe_results)
+@app.route("/insertIngredient")
+def insertIngredient():
+    ingredient_dict = {'name': request.args.get('ingredientName'),
+                       'quantity': request.args.get('ingredientQuantity'),
+                       'expiration_date': request.args.get('expirationDate')}
+    # save to database
+    db.create_all()
 
-@app.route("/getRecipeIngredients")
-def getRecipeIngredients():
-    recipe_id = request.args.get('recipe_id')
-    recipe_instructions = getRecipeInformation(recipe_id)
+    rand_int = random.randint(0, 999999999)
+    ingredient_object = Ingredient(id=rand_int,
+                                   ingredientName=ingredient_dict['name'],
+                                   ingredientQuantity=ingredient_dict['quantity'],
+                                   expirationDate=ingredient_dict['expiration_date'])
 
-    return render_template('recipeInstructions.html', results=recipe_instructions)
+    db.session.add(ingredient_object)
+    db.session.commit()
+
+    print(Ingredient.query.all())  # DEBUG
+    return render_template('submittedIngredient.html', results=ingredient_dict)
 
 
-# test module import
+def clearData(db):
+    meta = db.metadata
+    for table in reversed(meta.sorted_tables):
+        print('Clear table: %s' % table)
+        db.session.execute(table.delete())
+    db.session.commit()
+
+
 if __name__ == "__main__":
+    # test module import
     app.run(debug=True)
