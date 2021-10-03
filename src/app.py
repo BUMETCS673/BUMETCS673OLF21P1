01574618068b209af389895b3cee59f1f641bf05
grace303 <yanruz@bu.edu>
2021-10-03 10:01:38 -0700

    remove key

--- src/app.py
+++ src/app.py
@@ -5,8 +5,6 @@ from spoon import searchRecipes, get_recipe_by_id
 # flask class instance
 app = Flask(__name__)
 
-API_KEY = '9e749e7df97047c38000f0f4addb64f9'
-
 # / route will show our index template
 @app.route("/")
 def index():
