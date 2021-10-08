f85d832d102ed8f8959fff340a4f96e1d24b8e4e
Aidan Duffy <aduffy6144@gmail.com>
2021-10-08 14:44:48 -0400

    Added login button and changed routes for logging in and out to ensure functionality works

--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,5 @@
 from urllib.parse import urlencode
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, session
 from config import app
 from pantry import PantryManager
 from spoon import searchRecipes, getRecipeDetail
