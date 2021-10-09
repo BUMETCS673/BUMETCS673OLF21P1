67157d5a6294487e7136c596964ff57a69248c67
grace303 <yanruz@bu.edu>
2021-10-08 22:09:53 -0700

    Update selenium_tests.py

--- src/tests/selenium_tests.py
+++ src/tests/selenium_tests.py
@@ -1,3 +1,5 @@
+# docker-compose exec web python tests/selenium_tests.py
+
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
