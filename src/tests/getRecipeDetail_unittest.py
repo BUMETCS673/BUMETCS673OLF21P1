492df62fef77c96905f1ee2acce1734cc13205f0
grace303 <yanruz@bu.edu>
2021-10-10 12:19:35 -0700

    Update getRecipeDetail_unittest.py

--- src/tests/getRecipeDetail_unittest.py
+++ src/tests/getRecipeDetail_unittest.py
@@ -2,10 +2,11 @@
 Usage in docker: docker-compose exec web python tests/getRecipeDetail_unittest.py
 """
 
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import unittest
 import responses
 from spoon import getRecipeDetail
-
 
 class TestGetRecipeDetail(unittest.TestCase):
     @responses.activate
