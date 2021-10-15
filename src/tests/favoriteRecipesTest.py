50a5a8121107c7aa642e2fc3ade900026d43aea3
Kyle Mabry <68658192+kylemabry95@users.noreply.github.com>
2021-10-15 18:20:03 -0400

    Add more unit testing for favorite recipes

--- src/tests/favoriteRecipesTest.py
+++ src/tests/favoriteRecipesTest.py
@@ -10,16 +10,38 @@ from favoriteRecipes import FavoriteRecipeManager
 
 class TestFavorites(unittest.TestCase):
 
-    def testAddItem(self):
+    def testAddItemAndGetItem(self):
+        """Simultaneously tests addItem and
+        getItem from the favorites database"""
 
         fr = FavoriteRecipeManager("TestUser")
         fr.delFavAll()
-        fr.addFavoriteRecipe("9999", "Cheese Sandwich", "image/example")
+        fr.addFavoriteRecipe("9999", "Cheese Sandwich", "image/example.jpg")
         result = fr.getFavoriteRecipes()
-        print("ran")
-        print(result)
-        userID = result.split(" ")[0]
+        resultList = result.split(" ")
+        userID = resultList[0]
+        recipeName = " ".join(resultList[1:len(resultList) - 1])
+        recipeImage = resultList[-1]
+
         self.assertEqual("9999", userID)
+        self.assertEqual("Cheese Sandwich", recipeName)
+        self.assertEqual("image/example.jpg", recipeImage)
 
 
+    def testDelFavRecipe(self):
+        """Tests whether or not we can delete a
+        favorite recipe from the database"""
 
+        fr = FavoriteRecipeManager("TestUser")
+        fr.delFavAll()
+        fr.addFavoriteRecipe("9999", "Cheese Sandwich", "image/example.jpg")
+        fr.delFavoriteRecipe("9999")
+        result = fr.getFavoriteRecipes()
+        resultList = result.split(" ")
+        userID = resultList[0]
+        recipeName = " ".join(resultList[1:len(resultList) - 1])
+        recipeImage = resultList[-1]
+
+        self.assertEqual("", userID)
+        self.assertEqual("", recipeName)
+        self.assertEqual("", recipeImage)
